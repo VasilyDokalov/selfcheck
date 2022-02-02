48ec68218796be096965c5bcd46c9fd9efbf3900
hamster <dokalov@outlook.com>
2022-02-03 00:08:27 +0300

    page 134

---
+++ task027/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Comma
+{
+    static void Main()
+    {
+        int i, j;
+
+        for (i = 0, j = 10; i < j; i++, j--)
+            Console.WriteLine("i и j: " + i + " " + j);
+    }
+}
